d3ca85c494d4eed917e0fbfb162e48b8691a584b
NahuelGodoy <64289121+Jogana3@users.noreply.github.com>
2021-05-24 19:45:44 -0300

    Clase Sucursales

---
+++ TP8_GRUPO_2/Entidades/Sucursales.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Sucursales
+    {
+        private string _nombreSucursal;
+        private string _descripcion;
+        private int _IdProvincia;
+        private string _direccion;
+
+        public Sucursales()
+        {
+        }
+
+        public string NombreSucursal { get => _nombreSucursal; set => _nombreSucursal = value; }
+        public string Descripcion { get => _descripcion; set => _descripcion = value; }
+        public int IdProvincia { get => _IdProvincia; set => _IdProvincia = value; }
+        public string Direccion { get => _direccion; set => _direccion = value; }
+    }
+}
